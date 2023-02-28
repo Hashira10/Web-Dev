18cbe47ebc6d3b5c8158455ac3d3ead4c622a9b3
tenten <98640569+21BTen@users.noreply.github.com>
2023-02-28 21:17:26 +0600

    Add files via upload

---
+++ lab5/src/app/category.ts
@@ -0,0 +1,35 @@
+import {Product,products1} from "../../../my-app/src/app/products";
+
+export interface Category {
+  id: number;
+  name: string;
+}
+
+export const category1 = [
+  {
+    id: 1,
+    name: "Phones",
+
+  },
+  {
+    id: 2,
+    name: "E-books",
+
+  },
+
+  {
+    id: 3,
+    name: "Laptops",
+
+  },
+  {
+    id: 4,
+    name: "Bags",
+
+  },
+  {
+    id: 5,
+    name: "Tv",
+
+  },
+];
---
+++ lab5/src/app/products.ts
@@ -0,0 +1,201 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  url: string;
+  url1: string;
+  url2: string;
+  kaspi_link: string;
+  star1: string;
+  star2: string;
+  star3: string;
+  star4: string;
+  star5: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'ONYX BOOX Nova Air2',
+    price: 185000,
+    description: 'E-book ONYX BOOX Nova Air2 white,built-in backlight',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h78/h66/66012099215390/onyx-boox-nova-air2-belyi-107673318-1.jpg',
+
+    url1: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/he5/hb8/66012099739678/onyx-boox-nova-air2-belyi-107673318-2.jpg',
+
+    url2: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hb5/h22/66012100263966/onyx-boox-nova-air2-belyi-107673318-3.jpg',
+
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-boox-nova-air2-belyi-107673318/?c=750000000#!/item',
+    star1: "fa fa-star",
+    star2: "fa fa-star",
+    star3: "fa fa-star",
+    star4: "fa fa-star",
+    star5: "fa fa-star",
+  },
+  {
+    id: 2,
+    name: 'ONYX BOOX NOTE AIR 2 PLUS',
+    price: 255000,
+    description: 'E-book ONYX BOOX NOTE AIR 2 PLUS green ,built-in backlight',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hfe/h95/51932230451230/onyx-boox-note-air-2-plus-multikolor-105524843-1.jpg',
+
+    url1: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h60/hdf/61573115215902/onyx-boox-note-air-2-plus-multikolor-105524843-6.jpg',
+
+    url2: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h06/hb1/61573114298398/onyx-boox-note-air-2-plus-multikolor-105524843-4.jpg',
+
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-boox-note-air-2-plus-zelenyi-105524843/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
+  },
+
+  {
+    id: 3,
+    name: 'ONYX VASCO DA GAMA',
+    price: 70993,
+    description: 'E-book ONYX VASCO DA GAMA 4 black ,built-in backlight',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hdc/h58/66423830708254/onyx-vasco-da-gama-4-chernyi-107846588-1.jpg',
+
+    url1: '',
+
+    url2: '',
+
+
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-vasco-da-gama-4-chernyi-107846588/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
+  },
+  {
+    id: 4,
+    name: 'ONYX BOOX NOTE AIR 2 PLUS',
+    price: 179990,
+    description: 'E-book ONYX BOOX Poke 3 black,built-in backlight',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hcd/h67/46191145189406/onyx-boox-poke-3-cernyj-101138483-1-Container.jpg',
+
+    url1: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h52/h46/46191151906846/onyx-boox-poke-3-cernyj-101138483-2-Container.jpg',
+
+    url2: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/he5/h4b/46191208759326/onyx-boox-poke-3-cernyj-101138483-5-Container.jpg',
+
+
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-boox-poke-3-chernyi-101138483/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
+  },
+  {
+    id: 5,
+    name: 'ONYX BOOX Poke 4 Lite',
+    price: 85000,
+    description: 'ONYX BOOX Poke 4 Lite is a compact reader with a 6—inch E Ink Carta screen, with MOON Light 2 backlight and touch control. The model is made in a thin lightweight case, its weight is only 150 grams. The reader is based on a modern high-performance processor and has 2 GB of RAM and 16 GB of internal memory. The built-in Wi-Fi module allows you to access the Internet and download books directly to your device, and Bluetooth allows you to connect headphones, speakers, keyboards and much more.',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/he1/h1f/52430623047710/onyx-boox-poke-4-lite-belyj-105776884-1.jpg',
+
+    url1: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hd4/h5e/52430623277086/onyx-boox-poke-4-lite-belyj-105776884-2.jpg',
+
+    url2: '',
+
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-boox-poke-4-lite-belyi-105776884/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
+  },
+  {
+    id: 6,
+    name: 'PocketBook 606 PB606-D-CIS',
+    price: 74990,
+    description: 'PocketBook 606 is a new entry–level model with a 6-inch HD screen of the latest generation E Ink Carta and a record low weight, only 145 g. The reader is surprisingly easy to operate, safe for vision, supports 20 book and graphic formats and is equipped with a memory card slot. In short, PocketBook 606 has everything you need for comfortable electronic reading.',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h7e/h4c/33270743334942/pocketbook-606-pb606-d-cis-belyj-100665220-1-Container.jpg',
+
+    url1: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h9b/h38/52425524183070/pocketbook-606-pb606-d-cis-belyj-100665220-2-Container.jpg',
+
+    url2: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hc1/h56/33270762307614/pocketbook-606-pb606-d-cis-belyj-100665220-3-Container.jpg',
+
+    kaspi_link: 'https://kaspi.kz/shop/p/pocketbook-606-pb606-d-cis-belyi-100665220/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
+  },
+  {
+    id: 7,
+    name: 'Ritmix RBK-617',
+    price: 37228,
+    description: 'E-book Ritmix RBK-617 Black, built-in backlight',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h65/he9/33323904958494/ritmix-rbk-617-cernyj-100613290-1-Container.jpg',
+
+    url1: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h60/h16/33323920621598/ritmix-rbk-617-cernyj-100613290-2-Container.jpg',
+
+    url2: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h69/h5f/33323934285854/ritmix-rbk-617-cernyj-100613290-3-Container.jpg',
+
+    kaspi_link: 'https://kaspi.kz/shop/p/ritmix-rbk-617-chernyi-100613290/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star",
+  },
+  {
+    id: 8,
+    name: 'POCKET BOOK PB741-N-CIS',
+    price: 153990,
+    description: 'The worlds first large-format 7.8-inch E Ink color screen reader. Previously, all E Ink Kaleido color screen readers (E Ink Kaleido Plus) were 6-inch, and this model is 7.8-inch. The larger the diagonal of the screen, the more comfortable it is when studying specific content. Literature with pictures, the Internet in color, business publications with graphs and diagrams, comics – for all this, PocketBook 741 Color fits perfectly. A color reader, as convenient as possible for textbooks, business literature, comics.',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hda/h31/50916665131038/pocketbook-pb741-n-cis-serebristyj-101694825-1-Container.jpg',
+
+    url1: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hc6/h4c/50916665360414/pocketbook-pb741-n-cis-serebristyj-101694825-2-Container.jpg',
+
+    url2: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h50/h2c/50916897030174/pocket-book-pb741-n-cis-101694825-7.jpg',
+
+    kaspi_link: 'https://kaspi.kz/shop/p/pocket-book-pb741-n-cis-serebristyi-101694825/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
+  },
+  {
+    id: 9,
+    name: 'Amazon Kindle PaperWhite 2018 8Gb',
+    price: 99940,
+    description: 'The Amazon Kindle PaperWhite 2018 e–book is an updated version of the popular Amazon e-book. The thinnest and lightest reader of the Kindle Paperwhite line. The new model received a 6-inch E-Ink Carta screen with a resolution of 1440 x 1080. The display resembles real paper and does not glare even in the bright sun. Thanks to the built-in adjustable light, you can enjoy reading not only during the day, but also at night. The built-in memory is 8 GB. Amazon Kindle PaperWhite 2018 supports audiobooks. Using Bluetooth, you can connect headphones and listen to audiobooks through the headset. One battery charge for daily reading lasts for several weeks.',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h7d/h82/32330407575582/amazon-kindle-paperwhite-2018-8gb-sinij-100153809-1-Container.jpg',
+
+    url1: '',
+
+    url2: '',
+
+    kaspi_link: 'https://kaspi.kz/shop/p/amazon-kindle-paperwhite-2018-8gb-sinii-100153809/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
+  },
+  {
+    id: 10,
+    name: 'Amazon Kindle 11',
+    price: 73900,
+    description: 'Amazon Kindle E-book 11 Black, built-in backlight',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h3d/h9c/65742545420318/amazon-kindle-11-chernyi-107572855-1.jpg',
+
+    url1: '',
+
+    url2: '',
+
+    kaspi_link: 'https://kaspi.kz/shop/p/amazon-kindle-11-chernyi-107572855/?c=750000000#!/item',
+    star1: "fa fa-star",
+    star2: "fa fa-star",
+    star3: "fa fa-star",
+    star4: "fa fa-star",
+    star5: "fa fa-star",
+  },
+];
