0ae04fac749315ad56e9f6d42fb683d0da6e96ef
tenten <98640569+21BTen@users.noreply.github.com>
2023-03-01 20:06:47 +0600

    add products

--- lab5/src/app/category.ts
+++ lab5/src/app/category.ts
@@ -1,35 +1,487 @@
-import {Product,products1} from "../../../my-app/src/app/products";
-
-export interface Category {
+export interface Product {
   id: number;
+  likes: number;
+  category: string;
   name: string;
+  price: number;
+  description: string;
+  url: string;
+  url1: string;
+  url2: string;
+  kaspi_link: string;
+  star1: string;
+  star2: string;
+  star3: string;
+  star4: string;
+  star5: string;
 }
 
-export const category1 = [
+export const products = [
   {
     id: 1,
-    name: "Phones",
+    likes: 1,
+    category: "E-books",
+    name: 'ONYX BOOX Nova Air2',
+    price: 185000,
+    description: 'E-book ONYX BOOX Nova Air2 white,built-in backlight',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h78/h66/66012099215390/onyx-boox-nova-air2-belyi-107673318-1.jpg',
 
+    url1: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/he5/hb8/66012099739678/onyx-boox-nova-air2-belyi-107673318-2.jpg',
+
+    url2: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hb5/h22/66012100263966/onyx-boox-nova-air2-belyi-107673318-3.jpg',
+
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-boox-nova-air2-belyi-107673318/?c=750000000#!/item',
+    star1: "fa fa-star",
+    star2: "fa fa-star",
+    star3: "fa fa-star",
+    star4: "fa fa-star",
+    star5: "fa fa-star",
   },
   {
     id: 2,
-    name: "E-books",
+    likes: 1,
+    category: "E-books",
+    name: 'ONYX BOOX NOTE AIR 2 PLUS',
+    price: 255000,
+    description: 'E-book ONYX BOOX NOTE AIR 2 PLUS green ,built-in backlight',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hfe/h95/51932230451230/onyx-boox-note-air-2-plus-multikolor-105524843-1.jpg',
 
+    url1: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h60/hdf/61573115215902/onyx-boox-note-air-2-plus-multikolor-105524843-6.jpg',
+
+    url2: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h06/hb1/61573114298398/onyx-boox-note-air-2-plus-multikolor-105524843-4.jpg',
+
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-boox-note-air-2-plus-zelenyi-105524843/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
   },
 
   {
     id: 3,
-    name: "Laptops",
+    likes: 1,
+    category: "E-books",
+    name: 'ONYX VASCO DA GAMA',
+    price: 70993,
+    description: 'E-book ONYX VASCO DA GAMA 4 black ,built-in backlight',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hdc/h58/66423830708254/onyx-vasco-da-gama-4-chernyi-107846588-1.jpg',
 
+    url1: '',
+
+    url2: '',
+
+
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-vasco-da-gama-4-chernyi-107846588/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
   },
   {
     id: 4,
-    name: "Bags",
+    likes: 1,
+    category: "E-books",
+    name: 'ONYX BOOX NOTE AIR 2 PLUS',
+    price: 179990,
+    description: 'E-book ONYX BOOX Poke 3 black,built-in backlight',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hcd/h67/46191145189406/onyx-boox-poke-3-cernyj-101138483-1-Container.jpg',
 
+    url1: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h52/h46/46191151906846/onyx-boox-poke-3-cernyj-101138483-2-Container.jpg',
+
+    url2: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/he5/h4b/46191208759326/onyx-boox-poke-3-cernyj-101138483-5-Container.jpg',
+
+
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-boox-poke-3-chernyi-101138483/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
   },
   {
     id: 5,
-    name: "Tv",
+    likes: 1,
+    category: "E-books",
+    name: 'ONYX BOOX Poke 4 Lite',
+    price: 85000,
+    description: 'ONYX BOOX Poke 4 Lite is a compact reader with a 6—inch E Ink Carta screen, with MOON Light 2 backlight and touch control. The model is made in a thin lightweight case, its weight is only 150 grams. The reader is based on a modern high-performance processor and has 2 GB of RAM and 16 GB of internal memory. The built-in Wi-Fi module allows you to access the Internet and download books directly to your device, and Bluetooth allows you to connect headphones, speakers, keyboards and much more.',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/he1/h1f/52430623047710/onyx-boox-poke-4-lite-belyj-105776884-1.jpg',
 
+    url1: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hd4/h5e/52430623277086/onyx-boox-poke-4-lite-belyj-105776884-2.jpg',
+
+    url2: '',
+
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-boox-poke-4-lite-belyi-105776884/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
+  },
+  {
+    id: 6,
+    likes: 1,
+    category: "Phones",
+    name: 'Google Pixel 6A Smartphone 6GB/128GB Green',
+    price: 209900,
+    description: 'NFC technology: Yes\n' +
+      'Color: Green\n' +
+      'Screen type: Super AMOLED, capacitive, multi\n' +
+      '-touch diagonal: 6.2 inch\n' +
+      'RAM size: 6 GB\n' +
+      'Processor: 8 cores (2x2.80 GHz Cortex-X1 and 2x2.25 GHz Cortex-A76 and 4x1.80 GHz Cortex-A55)\n' +
+      'Internal memory: 128 GB\n' +
+      'Battery capacity: 4800mAh',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hdc/hbc/62084244209694/google-pixel-6a-6-gb-128-gb-zelenyi-106250320-1.jpg',
+    url1: '',
+    url2: '',
+    kaspi_link: 'https://kaspi.kz/shop/c/smartphones/brand-google/',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
+  },
+  {
+    id: 7,
+    likes: 1,
+    category: "Phones",
+    name: 'Google Pixel 6 8/128Gb Smartphone Black',
+    price: 258800,
+    description: 'NFC Technology: Yes\n' +
+      'Color: Black\n' +
+      'Screen Type: OLED, HDR10+, Gorilla Glass Victus\n' +
+      'Diagonal: 6.4 inch\n' +
+      'RAM size: 8 GB\n' +
+      'Processor: 8-core Google Tensor, Mali-G78\n' +
+      'Internal memory capacity: 128 GB\n' +
+      'Battery capacity: 4614 mAh',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hc4/h0c/47041827143710/google-pixel-6-8-128gb-cernyj-102811207-1.jpg',
+    url1: '',
+    url2: '',
+    kaspi_link: 'https://kaspi.kz/shop/p/google-pixel-6-8-128gb-chernyi-102811207/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star",
+  },
+  {
+    id: 8,
+    likes: 1,
+    category: "Phones",
+    name: 'Google Pixel 6A Smartphone 6GB/128GB Black',
+    price: 204900,
+    description: 'NFC Technology: Yes\n' +
+      'Color: Black\n' +
+      'Screen type: Super AMOLED, capacitive, multi\n' +
+      '-touch diagonal: 6.2 inch\n' +
+      'RAM size: 6 GB\n' +
+      'Processor: 8 cores (2x2.80 GHz Cortex-X1 and 2x2.25 GHz Cortex-A76 and 4x1.80 GHz Cortex-A55)\n' +
+      'Internal memory: 128 GB\n' +
+      'Battery capacity: 4800mAh',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h81/h12/62084243226654/google-pixel-6a-6-gb-128-gb-chernyi-106250318-1.jpg',
+    url1: '',
+    url2: '',
+    kaspi_link: 'https://kaspi.kz/shop/p/google-pixel-6a-6-gb-128-gb-chernyi-106250318/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
+  },
+  {
+    id: 9,
+    likes: 1,
+    category: "Phones",
+    name: 'Google Pixel 6a 6 Gb/128Gb',
+    price: 209900,
+    description: 'NFC Technology: Yes\n' +
+      'Color: white\n' +
+      'Screen type: Super AMOLED, capacitive, multi\n' +
+      '-touch diagonal: 6.2 inch\n' +
+      'RAM size: 6 GB\n' +
+      'Processor: 8 cores (2x2.80 GHz Cortex-X1 and 2x2.25 GHz Cortex-A76 and 4x1.80 GHz Cortex-A55)\n' +
+      'Internal memory: 128 GB\n' +
+      'Battery capacity: 4800mAh',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hd8/h97/62084242243614/google-pixel-6a-6-gb-128-gb-belyi-106250314-1.jpg',
+    url1: '',
+
+    url2: '',
+
+    kaspi_link: 'https://kaspi.kz/shop/p/google-pixel-6a-6-gb-128-gb-belyi-106250314/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
+  },
+  {
+    id: 10,
+    likes: 1,
+    category: 'Phones',
+    name: 'Google Pixel 7 Smartphone 8GB/128GB White',
+    price: 339900,
+    description: 'NFC technology: Yes\n' +
+      'Color: White\n' +
+      'Screen type: FHD+ (1080 x 2400) OLED\n' +
+      'Diagonal: 6.3 inch\n' +
+      'RAM size: 8 GB\n' +
+      'Processor: Google Tensor G2\n' +
+      'Internal memory capacity: 128 GB\n' +
+      'Battery capacity: 4355 mAh',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h03/he6/63411051954206/google-pixel-7-8-gb-128-gb-belyj-106971867-1.jpg',
+    url1: '',
+
+    url2: '',
+
+    kaspi_link: 'https://kaspi.kz/shop/p/google-pixel-7-8-gb-128-gb-belyi-106971867/?c=750000000#!/item',
+    star1: "fa fa-star",
+    star2: "fa fa-star",
+    star3: "fa fa-star",
+    star4: "fa fa-star",
+    star5: "fa fa-star",
+  },
+
+  {
+    id: 11,
+    likes: 1,
+    category: "Tv",
+    name: 'LG 43LM5772PLA ',
+    price: 178656,
+    description: 'Type: LED TV\n' +
+      'Diagonal: 42.5 inch\n' +
+      'Resolution: 1920x1080\n' +
+      'HD Support: 1080p Full HD\n' +
+      'Smart TV Technology: Yes\n' +
+      'Wi-Fi: Yes\n' +
+      'Inputs: AV, ,Component, ,HDMI, ,Ethernet (RJ-45), ,USB',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/he5/he4/49320430370846/lg-43lm5772pla-108-sm-cernyj-101293496-1-Container.jpg',
+    url1: '',
+    url2: '',
+    kaspi_link: 'https://kaspi.kz/shop/p/lg-43lm5772pla-109-sm-chernyi-101293496/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
+  },
+  {
+    id: 12,
+    likes: 1,
+    category: "Tv",
+    name: 'Xiaomi TV P1 32',
+    price: 37228,
+    description: 'Type: LED TV\n' +
+      'Diagonal: 32 inch\n' +
+      'Resolution: 1366x768\n' +
+      'HD Support: 720p HD\n' +
+      'Smart TV Technology: Yes\n' +
+      'Wi-Fi: Yes\n' +
+      'Inputs: Composite, ,HDMI, ,USB,,Bluetooth',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/haf/hd2/49320434008094/xiaomi-l32m6-6arg-81-sm-cernyj-103039169-1.jpg',
+    url1: '',
+    url2: '',
+    kaspi_link: 'https://kaspi.kz/shop/p/xiaomi-tv-p1-32-l32m6-6arg-81-sm-chernyi-103039169/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star",
+  },
+  {
+    id: 13,
+    likes: 1,
+    category: "Tv",
+    name: 'SSMART 43F20 109 TV',
+    price: 189900,
+    description: 'Type: LED TV\n' +
+      'Diagonal: 43 inch\n' +
+      'Resolution: 1920x1080\n' +
+      'HD Support: 1080p Full HD\n' +
+      'Smart TV Technology: Yes\n' +
+      'Wi-Fi: Yes\n' +
+      'Inputs: Audio, ,Optical, ,AV, ,Composite, ,VGA, ,HDMI, ,SCART, ,Ethernet (RJ-45), ,USB, ,antenna',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h37/h62/51432017985566/ssmart-43f20-chernyi-105159159-1.jpg',
+    url1: '',
+    url2: '',
+    kaspi_link: 'https://kaspi.kz/shop/p/pocket-book-pb741-n-cis-serebristyi-101694825/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
+  },
+  {
+    id: 14,
+    likes: 1,
+    category: "Tv",
+    name: 'TV Yasin 32G7 81 cm black',
+    price: 83305,
+    description: 'Type: LED TV\n' +
+      'Diagonal: 32 inch\n' +
+      'Resolution: 1280x780\n' +
+      'HD Support: 1080p Full HD\n' +
+      'Smart TV Technology: Yes\n' +
+      'Wi-Fi: Yes\n' +
+      'Inputs: AV, ,VGA, ,HDMI, ,USB',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h88/hc5/61768877965342/yasin-g7-cernyj-103489358-1.jpg',
+    url1: '',
+
+    url2: '',
+
+    kaspi_link: 'https://kaspi.kz/shop/p/yasin-32g7-81-sm-chernyi-103489358/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
+  },
+  {
+    id: 15,
+    likes: 1,
+    category: 'Tv',
+    name: 'Samsung UE43T5300AUXCE TV 109 cm Black',
+    price: 197940,
+    description: 'Type: LED TV\n' +
+      'Diagonal: 43 inch\n' +
+      'Resolution: 1920x1080\n' +
+      'HD Support: 1080p Full HD\n' +
+      'Smart TV Technology: Yes\n' +
+      'Wi-Fi: Yes\n' +
+      'Inputs: AV, ,Component, ,Ethernet (RJ-45), ,USB',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hdf/h18/49319748468766/samsung-ue43t5300au-chernyi-100182013-1.jpg',
+
+    url1: '',
+
+    url2: '',
+
+    kaspi_link: 'https://kaspi.kz/shop/p/samsung-ue43t5300auxce-109-sm-chernyi-100182013/?c=750000000#!/item',
+    star1: "fa fa-star",
+    star2: "fa fa-star",
+    star3: "fa fa-star",
+    star4: "fa fa-star",
+    star5: "fa fa-star",
+  },
+
+  {
+    id: 16,
+    likes: 1,
+    category: "Smart watch",
+    name: 'Amazfit Bip U Pro Smart Watch Black',
+    price: 25418,
+    description: 'Platform support: Android, ,iOS\n' +
+      'Case material: plastic\n' +
+      'Case color: black\n' +
+      'Screen technology: AMOLED\n' +
+      'Interfaces: Bluetooth, ,Wi-Fi, ,ANT+\n' +
+      'Working time: in active mode: 120 h, standby: 216 h',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h86/ha5/33549396574238/amazfit-bip-u-pro-cernyj-100914321-1-Container.jpg',
+    url1: '',
+    url2: '',
+    kaspi_link: 'https://kaspi.kz/shop/p/amazfit-bip-u-pro-chernyi-100914321/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
+  },
+  {
+    id: 17,
+    likes: 1,
+    category: "Smart watch",
+    name: 'Smart Watch Apple Watch SE 2nd Gen 40mm Golden',
+    price: 159890,
+    description: 'Platform support: iOS\n' +
+      'Case material: Aluminum\n' +
+      'Case color: gold\n' +
+      'Screen technology: OLED\n' +
+      'Internal memory capacity: 32 GB\n' +
+      'Interfaces: Bluetooth, ,Wi-Fi, ,NFC',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h97/h46/62711073407006/apple-watch-se-2nd-generation-40-mm-zolotistyj-106362759-1.jpg',
+    url1: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hd7/h19/62711073636382/apple-watch-se-2nd-generation-40-mm-zolotistyj-106362759-2.jpg',
+    url2: '',
+    kaspi_link: 'https://kaspi.kz/shop/p/apple-watch-se-2nd-gen-40-mm-zolotistyi-106362759/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star",
+  },
+  {
+    id: 18,
+    likes: 1,
+    category: "Smart watch",
+    name: 'Kieslect Smart Calling Whatch Kr Smart Watch Black',
+    price: 34988,
+    description: 'Platform support: Android, iOS\n' +
+      'Case material: metal\n' +
+      'Case color: Black\n' +
+      'Interfaces: Bluetooth\n' +
+      'Working time: 9 days/ when active - up to 2 days',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h5f/h83/62047586975774/xiaomi-kieslect-smart-calling-whatch-kr-chernyi-105772460-1.jpg',
+
+    url1: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hff/h15/62047587565598/xiaomi-kieslect-smart-calling-whatch-kr-chernyi-105772460-3.jpg',
+
+    url2: '',
+    kaspi_link: 'https://kaspi.kz/shop/p/kieslect-smart-calling-whatch-kr-chernyi-105772460/?c=750000000#!/item',
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
+  },
+  {
+    id: 19,
+    likes: 1,
+    category: "Smart watch",
+    name: 'Smart Watch COLMI P28 Plus Black',
+    price: 24990,
+    description: 'Platform support: Android,iOS\n' +
+      'Case color: Black\n' +
+      'Interfaces: Bluetooth\n' +
+      'Working time: 3-7 days',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hbb/he5/61766572539934/colmi-p28-plus-chernyi-106096505-1.jpg',
+
+    url1: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hb1/hf7/64634135314462/colmi-p28-plus-chernyi-106096505-4.jpg',
+
+    url2: '',
+
+    kaspi_link: 'https://kaspi.kz/shop/p/colmi-p28-plus-chernyi-106096505/?c=750000000#!/item' ,
+    star1: "fa fa-star checked",
+    star2: "fa fa-star checked",
+    star3: "fa fa-star checked",
+    star4: "fa fa-star checked",
+    star5: "fa fa-star checked",
+  },
+  {
+    id: 20,
+    likes: 1,
+    category: 'Smart watch',
+    name: 'Smart Watch Apple Watch Series 8 41mm Aluminum Gold',
+    price: 219970,
+    description: 'Platform support: iOS\n' +
+      'Case material: aluminum\n' +
+      'Case color: starlight\n' +
+      'Screen technology: OLED\n' +
+      'Internal memory capacity: 32 GB\n' +
+      'Interfaces: Bluetooth, ,Wi-Fi,,NFC\n' +
+      'Working time: in normal mode: 18 hours, in power saving mode: 36 hours',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h60/hfa/63158666461214/apple-watch-series-8-41-mm-aluminum-zolotistyj-106585035-1.jpg',
+
+    url1: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hdf/hd8/63158666690590/apple-watch-series-8-41-mm-aluminum-zolotistyj-106585035-2.jpg',
+
+    url2: '',
+
+    kaspi_link: 'https://kaspi.kz/shop/p/apple-watch-series-8-41-mm-aluminum-zolotistyi-106585035/?c=750000000#!/item',
+    star1: "fa fa-star",
+    star2: "fa fa-star",
+    star3: "fa fa-star",
+    star4: "fa fa-star",
+    star5: "fa fa-star",
   },
 ];
+
+
