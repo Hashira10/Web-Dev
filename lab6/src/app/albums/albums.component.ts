44314ec3efaf9589f89aeb4d5975bd43da0a2b26
tenten <98640569+21BTen@users.noreply.github.com>
2023-03-08 21:34:44 +0600

    add delete get albums functions

--- lab6/src/app/albums/albums.component.ts
+++ lab6/src/app/albums/albums.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { Album } from "../models";
 import { Albums} from "../fake-db";
+import {AlbumService} from "../album.service";
 
 @Component({
   selector: 'app-albums',
@@ -9,11 +10,40 @@ import { Albums} from "../fake-db";
 })
 export class AlbumsComponent implements OnInit{
   albums: Album[];
-  constructor() {
+  newAlbum: Album;
+  loaded: boolean;
+  delAlbum: Album;
+  constructor(private albumService: AlbumService) {
     this.albums = [];
+    this.newAlbum = {} as Album;
+    this.loaded = true;
+    this.delAlbum = {} as Album;
+  }
+  ngOnInit(): void {
+    this.getAlbums();
   }
 
-  ngOnInit(): void {
-    this.albums = Albums;
+  getAlbums(){
+    this.loaded = false;
+    this.albumService.getAlbums().subscribe((albums) =>{
+      this.albums = albums;
+      this.loaded = true;
+    })
   }
+
+  addAlbum(){
+    this.loaded = false;
+    this.albumService.addAlbum(this.newAlbum).subscribe((album) => {
+      // this.posts.push(post);
+      this.albums.unshift(album);
+      this.loaded = true;
+      this.newAlbum = {} as Album;
+    });
+  }
+
+  deleteAlbum(id:number){
+
+      this.albums = this.albums.filter(a => a.id !== id);
+  }
+
 }
